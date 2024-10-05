375c0d10e72f3650ef82ec75c3be49135087688d
Dave Barnwell <dave.barnwell@mac.com>
2024-10-05 17:08:17 +0100

    Initial Commit

---
+++ PublicIPToolbar/PublicIPToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublicIPToolbarApp.swift
+//  PublicIPToolbar
+//
+//  Created by Dave Barnwell on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PublicIPToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PublicIPToolbarTests/PublicIPToolbarTests.swift
@@ -0,0 +1,17 @@
+//
+//  PublicIPToolbarTests.swift
+//  PublicIPToolbarTests
+//
+//  Created by Dave Barnwell on 05/10/2024.
+//
+
+import Testing
+@testable import PublicIPToolbar
+
+struct PublicIPToolbarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
